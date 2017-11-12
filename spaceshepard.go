6976e49b08a5161859cad71a16f7648311400b92
Dario Maiocchi <dmaiocchi@suse.com>
2017-11-12 21:28:54 +0100

    init

--- spaceshepard.go
+++ spaceshepard.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"github.com/MalloZup/spaceshepard/ssh"
+    "bytes"
+    "log"
+    "os/exec"
 )
 
+
+
+
 func main() {
-	fmt.Printf("SpaceShepard.\n")
-	ssh.Run("uptime")
+    var remoteMachine  = "root@localhost"
+    var out bytes.Buffer
+    cmd := exec.Command("ssh", remoteMachine, "uptime")
+    cmd.Stdout = &out
+    err := cmd.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
 }
